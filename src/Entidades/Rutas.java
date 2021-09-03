cba72d5b0557c40de59b2afc4fdb731bd5a19ff0
LuisRomero19 <luis.romero20@unmsm.edu.pe>
2021-09-03 07:39:50 -0500

    correccion bd

--- src/Entidades/Rutas.java
+++ src/Entidades/Rutas.java
@@ -1,8 +1,8 @@
 package Entidades;
 
 public abstract class Rutas {
-    public static String R_LIBROS = "\\BaseDeDatos\\Libros.tecnomedic";
-    public static String R_BIBLIOTECARIOS = "\\BaseDeDatos\\Bibliotecarios.tecnomedic";
-    public static String R_USUARIOS = "\\BaseDeDatos\\Usuarios.tecnomedic";
+    public static String R_LIBROS = "\\BaseDeDatos\\Libros.fisitk";
+    public static String R_BIBLIOTECARIOS = "\\BaseDeDatos\\Bibliotecarios.fisitk";
+    public static String R_USUARIOS = "\\BaseDeDatos\\Usuarios.fisitk";
     
 }
